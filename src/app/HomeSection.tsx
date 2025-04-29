2685e7a2f78673b538678c8ef23aeed3d4f98036
sunpochin <sunpochin@gmail.com>
2025-04-29 18:51:42 +0800

    feat: 1.use serif font 2. some circles in the place

--- src/app/HomeSection.tsx
+++ src/app/HomeSection.tsx
@@ -1,5 +1,6 @@
 // src/app/HomeSection.tsx
 import React from 'react';
+import Navbar from '@/app/NavBar';
 
 const HomeSection = () => {
   const circles = Array.from({ length: 10 }).map((_, index) => {
@@ -11,7 +12,7 @@ const HomeSection = () => {
       <div
         key={index}
         className="circle"
-        style={{ left: `${left}vw`, top: `${top}vh` }} // use vw and vh to set position
+        style={{ left: `${left}%`, top: `${top}%` }} // use percentage to set position
       />
     );
   });
@@ -20,17 +21,17 @@ const HomeSection = () => {
     <section id="home" className="
     min-h-screen min-w-screen bg-custom-pink
     relative">
-      <h1>Home</h1>
+      <Navbar />
       <img src="/images/tomoroll.png" 
         alt="tomoroll" 
         className="w-1/3 absolute top-0 left-20" />
-      <div className="flex flex-col items-center justify-center mt-20">
+      <div className="flex flex-col items-center justify-between mt-20">
         <img src="/images/whisky-1920.jpg" alt="home-image" />
-        <div className="font-custom-brown text-center text-4xl w-1/2 h-screen">
-          Small ideas we grasp from a daydream of an elegant life
+        <div id="ideas" className="font-custom-brown text-center text-4xl w-1/2 min-h-screen flex flex-col items-center justify-center relative">
+          <p>Small ideas we grasp from a daydream of an elegant life</p>
+          {circles}
         </div>
       </div>
-      {circles}
     </section>
   );
 };
