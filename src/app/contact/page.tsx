192b9167c41f26b0f9b85fee88fa6f2ec1ad3bef
sunpochin <sunpochin@gmail.com>
2025-05-08 07:47:09 +0800

    fix: add back the contact page I forgot to add

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+// src/app/ContactPage.tsx
+import React from 'react'
+import ContactSection from '@/app/ContactSection'
+
+const ContactPage = () => {
+  return (
+    <div>
+      <ContactSection />
+    </div>
+  )
+}
+
+export default ContactPage
